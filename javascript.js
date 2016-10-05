8e9b320bbe666c66209017022076750245e0ae47
Scott McCord <scott.sdm27@gmail.com>
2016-10-05 13:26:38 -0400

    created non-functional draft of grid creation

--- javascript.js
+++ javascript.js
@@ -1,4 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 console.log("Puzzle-d!");
-
 });
+
+// create the game state:
+
+var grid = [];
+
+var createGrid = function() {
+  for (var i = 0; i <= WidthOfBoard; i += 50) {
+    for (var j = 0; j <= heightOfBoard; j += 50) {
+      grid[i][j] = {
+        x: i;
+        y: j;
+      }
+    }
+  }
+}
