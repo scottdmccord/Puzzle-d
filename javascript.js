acc02571f0225078840dd91c0fbe65f40ef31372
Scott McCord <scott.sdm27@gmail.com>
2016-10-05 17:05:48 -0400

    divs loaded to page

--- javascript.js
+++ javascript.js
@@ -1,31 +1,43 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-console.log("Puzzle-d!");
 
-});
 
 // create the game state:
 
 var grid = [];
 
 var createGrid = function() {
-  for (var i = 0; i < 5; i++) {
+  for (var i = 0; i < 12; i++) {
     grid[i] =[];
-    for (var j = 0; j < 5; j++) {
-      grid[i][j] = { x: (j * 50), y: (i * 50)}
-      }
+    for (var j = 0; j < 18; j++) {
+      grid[i][j] = { x: (j * 50), y: (i * 50)};
     }
   }
+}
 
 createGrid();
 
-var $container = $('container');
+// after DOM loads
+document.addEventListener("DOMContentLoaded", function(event) {
+console.log("Puzzle-d!");
 
+var $container = $('.container');
 var createBoard = function() {
-  for(var i = 0; i < grid.length; i++) {
-    var $piece = $('<div>');
-    $piece.addClass('piece');
-    $piece.css({
-      'background': 'url('
-    })
+  var counter = 0;
+  for (var a = 0; a < grid.length; a++) {
+       for(var b  = 0; b < grid[a].length; b++) {
+         var $block = $('<div>');
+         $block.addClass('piece');
+         $block.attr('id' , 'piece' + counter);
+         $block.css({
+           'background-position' : grid[a][b].x+'px '+grid[a][b].y+'px'
+          })
+         $container.append($block);
+         counter++;
+      }
   }
 }
+
+createBoard();
+
+});
+
+
